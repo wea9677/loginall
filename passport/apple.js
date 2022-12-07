1a8cb28e1a9815a349dd2e950f6c16504ad0ef4f
Ginam <wea9677@naver.com>
2022-12-07 11:25:45 +0900

    apple passport 설정

--- passport/apple.js
+++ passport/apple.js
@@ -1,26 +1,43 @@
 const passport = require('passport');
 const AppleStrategy = require('passport-apple').Strategy;
 // const user = require('../model/user');
-
-module.exports = () =>{
+const fs = require('fs');
+module.exports = () => {
     passport.use(
         new AppleStrategy(
             {
-                clientID: 'com.herokuapp.applepassport-web',
+                clientID: 'app.koyeb.loginall',
                 teamId:'3L7RW74HCJ',
                 callbackURL: 'loginall-wea9677.koyeb.app/auth/apple', 
-                keyID: '79KCA9TG7S',
-                privatekeyLocation: fs.readFileSync('./AuthKey_79KCA9TG7S.p8'),
+                keyID: 'X6WB7MSR33',
+                privatekeyLocation: fs.readFileSync('./AuthKey_X6WB7MSR33.p8'),
                 scope: 'name email'
             }, async (req, accessToken, refreshToken, idToken, profile, cb) =>{
                 console.log('데이터 지니가나요',
                     idToken, accessToken, refreshToken, profile
-                )
-            }
-        )
-    )
+                );
+                try {
+                    const exUser = await user.findOne({
+                        where: {userId:idToken, provider : 'apple'},
+                    });
 
+                    if(exUser) {
+                        done(null, exUser);
 
+                    }else{
+                        const newUser = await user.create({
+                            userId : idToken.sub,
+                            email : idToken.email
+                        });
+                        done(null, newUser);
+                    }
+                } catch (error) {
+                    console.error(error);
+                    done(error);
+                }
+            },
+        ),
+    );
 
 
 };
