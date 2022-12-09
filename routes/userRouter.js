3ef4e41bfdc8ed39805954629eee514b1ac2eb07
Ginam <wea9677@naver.com>
2022-12-09 17:01:21 +0900

    bodyParser 추가

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
-
+const bodyParser = require('body-parser');
 const {
     googleCallback,
     appleCallback,
