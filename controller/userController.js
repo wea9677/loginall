ad95ecfb2ca938d543f2c2c31b92c50f5dcd848f
Ginam <wea9677@naver.com>
2022-12-13 20:25:13 +0900

    되는건가

--- controller/userController.js
+++ controller/userController.js
@@ -101,11 +101,12 @@ const apple_auth =  async ( req, res, next) =>{
         });
         res.send(newUser);
         console.log(newUser, '뉴유저');
+        if (exUser){
         const exUser = await user.findOne({
            $or:[{userId:idToken.sub, provider:'apple'}],
         });
         console.log(exUser, '이미 있을지 확인')
-        if (exUser){
+        
             const {userId, email} = user;
             const token = jwt.sign({userId}, process.env.MY_KEY, {
                 expiresIn:'24h',
