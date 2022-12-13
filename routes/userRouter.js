6171721c3c935974dd6ecab1184d24afe3fcafea
Ginam <wea9677@naver.com>
2022-12-13 13:44:14 +0900

    경로수정

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -24,9 +24,9 @@ router.get('/google/callback', googleCallback);
 // 애플 로그인 apple-auth
 router.post('/apple/callback', bodyParser(), apple_auth );
 
-router.get('/token', tokenG);
+router.get('/apple', tokenG);
 
-router.get('/refresh', apple_refresh );
+router.get('/apple/refresh', apple_refresh );
 
 
 //로컬 회원가입 & 로그인
